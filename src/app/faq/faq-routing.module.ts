4906eadf7f2a09f2d236d5aeec08e71be1ecee29
Davis Chan <davischan@ud.hk>
2022-03-05 16:50:00 +0800

    feat(faq): finished faq page ui

--- src/app/faq/faq-routing.module.ts
+++ src/app/faq/faq-routing.module.ts
@@ -1,10 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FaqComponent} from './faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'new-to-angular',
+		pathMatch: 'full',
+		component: FaqComponent
+	},
+	{
+		path: 'why-this-book',
+		pathMatch: 'full',
+		component: FaqComponent
+	},
+	{
+		path: 'how-to-support',
+		pathMatch: 'full',
+		component: FaqComponent
+	},
+	{
+		path: 'contribution',
+		pathMatch: 'full',
+		component: FaqComponent
+	},
+	{path: '**', redirectTo: '/'}
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
-export class FaqRoutingModule { }
+export class FaqRoutingModule {
+}
